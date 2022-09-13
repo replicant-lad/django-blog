34a1296a8977d0d0240d74dcf3a9434b4f5da2f7
replicant-lad <douglasldexter@gmail.com>
2022-09-12 20:11:22 -0600

    Third Lesson

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author' : 'dogbot',
+        'title' : 'blogpost 1',
+        'content' : 'bush did 7/11',
+        'date_posted' : 'September 12th, 2022'
+    },
+    {
+        'author' : 'nutbot',
+        'title' : 'blogpost 2',
+        'content' : 'obama did 4/20',
+        'date_posted': 'September 13th, 2022'
+    },
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About</h2>')
+    return render(request, 'blog/about.html', {'title': 'About'})
 
